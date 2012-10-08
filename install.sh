7a09fa04f731667c44db7a5803c783d9a07cdd38
janul <janusz.ulanowski@heanet.ie>
2012-10-08 14:58:17 +0100

    check if required commands exist in the system

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+#if you use different tool than mktemp then pls find TMPDIR below and change option
+MKTEMPTOOL="mktemp"
+
+command -v ${MKTEMPTOOL} >/dev/null 2>&1 || { echo >&2 "${MKTEMPTOOL}  is required but it's not installed.  Some distributions have tempfile. If so pls edit this file and change line containing MKTEMPTOOL def. Aborting."; exit 1; }
+command -v tar >/dev/null 2>&1 || { echo >&2 "tar is required but it's not installed.  Aborting."; exit 1; }
+command -v wget >/dev/null 2>&1 || { echo >&2 "wget is required but it's not installed.  Aborting."; exit 1; }
 WELCOMEMSG="Script will create additional folders and downloads additional software"
 echo -e ${WELCOMEMSG}
-
-
 install(){
-   TMPDIR=`mktemp -d`
+   TMPDIR=`${MKTEMPTOOL} -d`
    LOGOS="logos2"
    if [ ! -d "${LOGOS}" ]
    then
