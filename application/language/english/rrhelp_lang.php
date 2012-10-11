eaa98ec34b1d8b5a6473b546bbeb3843cfe22537
janul <janusz.ulanowski@heanet.ie>
2012-10-11 11:59:37 +0100

    rm edugate name from help

--- application/language/english/rrhelp_lang.php
+++ application/language/english/rrhelp_lang.php
@@ -4,9 +4,9 @@
 $lang['rhelp_entityid'] = 'Use a URL https://<HOSTNAME>/idp/shibboleth. This URL does not necessarily need to resolve to a web page yet, but it should be possible publish your IDPs own metadata at this URL (Shibboleth 2.1+ IdP metadata is usually automatically published at https://idp.example.org/idp/profile/Metadata/SAML).';
 
 $lang['rhelp_resourcename'] = 'Name of the Resource or Service';
-$lang['rhelp_helpdeskurl'] = 'A web page that offers users help and guidance in case of Edugate related problems with the resource';
+$lang['rhelp_helpdeskurl'] = 'A web page that offers users help and guidance in case of Federation related problems with the resource';
 $lang['rhelp_assertionconsumer'] = 'Define bindings that your service provider supports';
-$lang['rhelp_acsurl'] = 'ACS URL';
+$lang['rhelp_acsurl'] = 'AttributeConsumerService URL';
 $lang['rhelp_certificatesigning'] = 'Certificate for signing';
 $lang['rhelp_certificateencryption'] = 'Certificate for encryption';
 
