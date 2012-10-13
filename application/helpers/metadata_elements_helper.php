ada8fd1ec2259c891ef36896ff5e85b8e69879ee
janul <janusz.ulanowski@gmail.com>
2012-10-13 23:25:30 +0200

    Update application/helpers/metadata_elements_helper.php

--- application/helpers/metadata_elements_helper.php
+++ application/helpers/metadata_elements_helper.php
@@ -28,6 +28,7 @@ function h_metadataNamespaces()
          'saml'=>'urn:oasis:names:tc:SAML:2.0:assertion',
          'shibmd'=>'urn:mace:shibboleth:metadata:1.0',
          'mdui'=>'urn:oasis:names:tc:SAML:metadata:ui',
+         'mdrpi'=>'urn:oasis:names:tc:SAML:metadata:rpi',
          'idpdisc'=>'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol',
          'wayf'=>'http://sdss.ac.uk/2006/06/WAYF',
          'elab'=>'http://eduserv.org.uk/labels',
