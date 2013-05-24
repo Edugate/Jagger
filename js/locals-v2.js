6c0ac5cae65586b95a09b3af3c14e2d726c76963
janul <janusz.ulanowski@heanet.ie>
2013-05-24 16:01:09 +0100

    js f

--- js/locals-v2.js
+++ js/locals-v2.js
@@ -227,8 +227,8 @@ $(function() {
         $.ajax({
             url: url,
             timeout: 2500,
-            cache: false,
-        }).done(function() { setTimeout('go_to_private_page()', 1000); });
+            cache: false
+        }).done(function(){ setTimeout('go_to_private_page()', 1000);});
 
         return false;
     });
