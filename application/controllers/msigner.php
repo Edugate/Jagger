0e3dc3114d069fb3935256d6eaa6f785f7130969
janul <janusz.ulanowski@heanet.ie>
2013-07-16 15:44:59 +0100

    fix condition

--- application/controllers/msigner.php
+++ application/controllers/msigner.php
@@ -45,7 +45,8 @@ class Msigner extends MY_Controller {
        {
            show_error('empty tyle or id: '.lang('error404').'',404);
        }
-       if(empty($this->config->item('gearman')))
+       $gearmanenabled = $this->config->item('gearman')
+       if(empty($gearmanenabled))
        {
            show_error('gearman is not enabled '.lang('error404').'',404);
        }       
