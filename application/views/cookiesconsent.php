97b432ccc47c44f3030653db1c5257197aae45b7
janul <janusz.ulanowski@heanet.ie>
2014-06-25 16:19:56 +0100

    cookieconsent fix

--- application/views/cookiesconsent.php
+++ application/views/cookiesconsent.php
@@ -1,10 +1,10 @@
-<div id="cookiesinfo"  data-alert class="alert-box notice">
-<div id="lcookiesinfo">
+<div id="cookiesinfo" >
+<div id="lcookiesinfo" data-alert class="alert-box notice">
 <?php
 echo $value;
 
 ?>
 </div>
-<div id="rcookiesinfo" ><a href="<?php echo base_url().'ajax/consentCookies'; ?>" class="pCookieAccept"><button type="button" class="savebutton saveicon button tiny"><?php echo lang('rr_readnotice');?></button></a></div>
+<div id="rcookiesinfo"  class="small-12 columns text-right"><a href="<?php echo base_url().'ajax/consentCookies'; ?>" class="pCookieAccept"><button type="button" class="savebutton saveicon button tiny"><?php echo lang('rr_readnotice');?></button></a></div>
 <div id="cookiefoot"></div>
 </div>
