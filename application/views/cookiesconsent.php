e43caf1ed33c38a61b4442f50d2b480f4f3081e1
janul <janusz.ulanowski@heanet.ie>
2014-06-25 16:16:14 +0100

    cookieconsent

--- application/views/cookiesconsent.php
+++ application/views/cookiesconsent.php
@@ -1,10 +1,10 @@
-<div id="cookiesinfo">
+<div id="cookiesinfo"  data-alert class="alert-box notice">
 <div id="lcookiesinfo">
 <?php
 echo $value;
 
 ?>
 </div>
-<div id="rcookiesinfo"><a href="<?php echo base_url().'ajax/consentCookies'; ?>" class="pCookieAccept"><button type="button" class="savebutton saveicon"><?php echo lang('rr_readnotice');?></button></a></div>
+<div id="rcookiesinfo" ><a href="<?php echo base_url().'ajax/consentCookies'; ?>" class="pCookieAccept"><button type="button" class="savebutton saveicon button tiny"><?php echo lang('rr_readnotice');?></button></a></div>
 <div id="cookiefoot"></div>
 </div>
