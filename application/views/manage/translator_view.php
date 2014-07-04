d70de9539c0c293a114fb9d082d06ad42e4eb257
janul <janusz.ulanowski@heanet.ie>
2014-07-04 16:35:55 +0100

    fix translator view

--- application/views/manage/translator_view.php
+++ application/views/manage/translator_view.php
@@ -5,13 +5,18 @@ echo form_fieldset();
 echo '<ul style="list-style-type: none;">';
 foreach($merger as $k=>$v)
 {
-   if($v['english'] == $v['to'])
+   if(!isset($v['english']))
+   {
+       unset($merger[$k]);
+       continue;
+   }   
+   if(strcasecmp($v['english'], $v['to']) == 0)
    {
       $bgcolor = 'yellow';
    }
    else
    {
-      $bgcolor = 'transparent';
+      $bgcolor = 'white';
    }
    echo '<li style="margin: 5px;background-color: #cfcfcf;"><label style="text-align: left; font-weight: bolder" for="lang['.$k.']" style="word-break: break-word">'.$v['english'].' <span style="font-weight: normal;"><small>['.$k.']</small><span></label>';
    
@@ -21,7 +26,7 @@ foreach($merger as $k=>$v)
 }
 
 echo '</ul>';
-echo '<input class="savebutton saveicon" type="submit" value="submit" name="submit">';
+echo '<input class="savebutton saveicon button" type="submit" value="submit" name="submit">';
 echo form_fieldset_close();
 echo form_close();
 echo '</div>';
