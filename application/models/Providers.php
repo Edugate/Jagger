e238c886a5cd91b32e9d7ebc2c7b9d3631894e06
janul <janusz.ulanowski@heanet.ie>
2014-09-08 16:45:44 +0100

    fixes

--- application/models/Providers.php
+++ application/models/Providers.php
@@ -2,7 +2,8 @@
 
 namespace models;
 
-if (!defined('BASEPATH')) exit('No direct script access allowed');
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 use \Doctrine\Common\Collections\ArrayCollection;
 use \Doctrine\ORM\Query\ResultSetMapping;
@@ -24,8 +25,7 @@ use \Doctrine\ORM\Query\ResultSetMapping;
  * @subpackage  Models
  * @author      Janusz Ulanowski <janusz.ulanowski@heanet.ie>
  */
-class Providers
-{
+class Providers {
 
     protected $providers;
     protected $em;
@@ -42,29 +42,29 @@ class Providers
     {
         $this->providers = new \Doctrine\Common\Collections\ArrayCollection();
         $federations = $provider->getFederations();
-        foreach ($federations->getValues() as $f) {
+        foreach ($federations->getValues() as $f)
+        {
             $y = $f->getMembers();
-            foreach ($y->getKeys() as $key) {
+            foreach ($y->getKeys() as $key)
+            {
                 $this->providers->set($key, $y->get($key));
             }
         }
         return $this->providers;
     }
 
- 
     public function getTrustedActiveFeds(Provider $provider)
     {
         $feds = new \Doctrine\Common\Collections\ArrayCollection();
         $provid = $provider->getId();
-        $query = $this->em->createQuery("SELECT m,f FROM models\FederationMembers m JOIN m.federation f WHERE m.provider = '".$provid."' AND m.joinstate != '2' AND m.isDisabled = '0' AND m.isBanned='0' AND f.is_active = '1'");
+        $query = $this->em->createQuery("SELECT m,f FROM models\FederationMembers m JOIN m.federation f WHERE m.provider = '" . $provid . "' AND m.joinstate != '2' AND m.isDisabled = '0' AND m.isBanned='0' AND f.is_active = '1'");
         //$query->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
         $result = $query->getResult();
-        foreach($result as $r)
+        foreach ($result as $r)
         {
-           $feds->add($r->getFederation());
+            $feds->add($r->getFederation());
         }
         return $feds;
-        
     }
 
     public function getSPsForArp(Provider $provider)
@@ -74,27 +74,26 @@ class Providers
         $query1->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
         $result1 = $query1->getResult();
         $feds = array();
-        foreach($result1 as $r)
+        foreach ($result1 as $r)
         {
-           $feds[] = $r->getFederation()->getId();
+            $feds[] = $r->getFederation()->getId();
         }
-        if (count($feds) == 0) {
-           return array();
-        }      
+        if (count($feds) == 0)
+        {
+            return array();
+        }
         $query = $this->em->createQuery("SELECT p,e,m,f FROM models\Provider p LEFT JOIN p.membership m LEFT JOIN m.federation f LEFT JOIN p.extend e WHERE m.federation IN (:feds) AND  m.joinstate != '2' AND m.isDisabled = '0' AND m.isBanned='0' AND p.id != ?2 AND p.is_active = '1' AND p.is_approved = '1' AND p.type IN ('SP','BOTH')");
-        $query->setParameter('feds',$feds);
-        $query->setParameter(2,$provider->getId());
+        $query->setParameter('feds', $feds);
+        $query->setParameter(2, $provider->getId());
         $query->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
         $result = $query->getResult();
         $r2 = new \Doctrine\Common\Collections\ArrayCollection;
-        foreach($result as $r)
+        foreach ($result as $r)
         {
-           $r2->add($r);
+            $r2->add($r);
         }
         return $r2;
-          
     }
-   
 
     public function getIdPsForWayf(Provider $provider)
     {
@@ -103,20 +102,20 @@ class Providers
         $query1->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
         $result1 = $query1->getResult();
         $feds = array();
-        foreach($result1 as $r)
+        foreach ($result1 as $r)
         {
-           $feds[] = $r->getFederation()->getId();
+            $feds[] = $r->getFederation()->getId();
         }
-        if (count($feds) == 0) {
-           return array();
-       }
-        $fedin = implode(',', $feds);
-        $query = $this->em->createQuery("SELECT p,e,m FROM models\Provider p LEFT JOIN p.extend e LEFT JOIN p.membership m LEFT JOIN m.federation f  WHERE m.federation IN (" . $fedin . ") AND  m.joinstate != '2' AND m.isDisabled = '0' AND m.isBanned='0' AND p.id != ?2 AND p.is_active = '1' AND p.is_approved = '1' AND p.type IN ('IDP','BOTH')");
-        $query->setParameter(2,$provider->getId());
+        if (count($feds) == 0)
+        {
+            return array();
+        }
+        $query = $this->em->createQuery("SELECT p,e,m FROM models\Provider p LEFT JOIN p.extend e LEFT JOIN p.membership m LEFT JOIN m.federation f  WHERE m.federation IN (:feds) AND  m.joinstate != '2' AND m.isDisabled = '0' AND m.isBanned='0' AND p.id != ?2 AND p.is_active = '1' AND p.is_approved = '1' AND p.type IN ('IDP','BOTH')");
+        $query->setParameter('feds', $feds);
+        $query->setParameter(2, $provider->getId());
         $query->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
         $result = $query->getResult();
         return $result;
-          
     }
 
     /**
@@ -130,33 +129,43 @@ class Providers
         $entype = $provider->getType();
         $membership = $provider->getMembership();
         $feds = array();
-        foreach ($membership as $m) {
+        foreach ($membership as $m)
+        {
             $fullTrustEnabled = $m->getIsFinalMembership();
-            if ($excludeDisabledFeds) {
+            if ($excludeDisabledFeds)
+            {
                 $isFedEnabled = TRUE;
             }
-            else {
+            else
+            {
                 $isFedEnabled = $m->getFederation()->getActive();
             }
-            if ($fullTrustEnabled && $isFedEnabled) {
+            if ($fullTrustEnabled && $isFedEnabled)
+            {
                 $feds[] = $m->getFederation()->getId();
             }
         }
-        if (count($feds) == 0) {
+        if (count($feds) == 0)
+        {
             return array();
         }
 
         $fedmembers = $this->em->getRepository("models\FederationMembers")->findBy(array('federation' => $feds, 'isDisabled' => FALSE, 'isBanned' => FALSE, 'joinstate' => array('0', '1', '3')));
-        if ($entype === 'IDP' or $entype === 'SP') {
-            foreach ($fedmembers as $m) {
+        if ($entype === 'IDP' or $entype === 'SP')
+        {
+            foreach ($fedmembers as $m)
+            {
                 $pr = $m->getProvider();
-                if (strcmp($pr->getType(), $entype) != 0) {
+                if (strcmp($pr->getType(), $entype) != 0)
+                {
                     $trustedEntities->add($pr);
                 }
             }
         }
-        else {
-            foreach ($fedmembers as $m) {
+        else
+        {
+            foreach ($fedmembers as $m)
+            {
                 $trustedEntities->add($m->getProvider());
             }
         }
@@ -171,38 +180,42 @@ class Providers
     {
         $type = $provider->getType();
         $rtype = array();
-        if ($type === 'IDP') {
+        if ($type === 'IDP')
+        {
             $typeconds = "'SP','BOTH'";
             $rtype = array('SP', 'BOTH');
         }
-        elseif ($type === 'SP') {
+        elseif ($type === 'SP')
+        {
             $typeconds = "'IDP','BOTH'";
             $rtype = array('IDP', 'BOTH');
         }
-        else {
+        else
+        {
             $typeconds = "'IDP','SP','BOTH'";
             $rtype = array('IDP', 'SP', 'BOTH');
         }
         $feds = array();
         $membership = $provider->getMembership();
-        foreach ($membership as $m) {
+        foreach ($membership as $m)
+        {
             \log_message('debug', 'GKS2: disabled: ' . (int) $m->getIsDisabled() . ' : ' . $m->getFederation()->getName());
             $fullyEnabled = $m->getIsFinalMembership();
             \log_message('debug', 'GKS2: fullyEnabled: ' . $fullyEnabled . ' : ' . $m->getFederation()->getName());
             $federationEnabled = $m->getFederation()->getActive();
-            if ($fullyEnabled && $federationEnabled) {
+            if ($fullyEnabled && $federationEnabled)
+            {
                 $feds[] = '' . $m->getFederation()->getId() . '';
             }
         }
-        if (count($feds) == 0) {
+        if (count($feds) == 0)
+        {
             return array();
         }
-        $fedin = implode(',', $feds);
-
-
-        $query = $this->em->createQuery("SELECT p,m, f FROM models\Provider p JOIN p.membership m JOIN m.federation f  WHERE m.federation IN (" . $fedin . ") AND  m.joinstate != '2' AND m.isDisabled = '0' AND m.isBanned='0' AND p.id != 2? AND p.is_active = '1' AND p.is_approved = '1' AND p.type IN (" . $typeconds . ")");
-        $query->setParameter(2,$provider->getId());
-       // $query->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
+        $query = $this->em->createQuery("SELECT p,m, f FROM models\Provider p JOIN p.membership m JOIN m.federation f  WHERE m.federation IN (:feds) AND  m.joinstate != '2' AND m.isDisabled = '0' AND m.isBanned='0' AND p.id != 2? AND p.is_active = '1' AND p.is_approved = '1' AND p.type IN (" . $typeconds . ")");
+        $query->setParameter('feds', $feds);
+        $query->setParameter(2, $provider->getId());
+        // $query->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
         $result = $query->getResult();
         return $result;
     }
@@ -211,38 +224,39 @@ class Providers
     {
         $type = $provider->getType();
         $types = array();
-        if($type === 'IDP')
+        if ($type === 'IDP')
         {
-           $types = array('\'SP\'','\'BOTH\'');
+            $types = array('SP', 'BOTH');
         }
-        elseif($type === 'SP')
+        elseif ($type === 'SP')
         {
-           $types = array('\'IDP\'','\'BOTH\'');
+            $types = array('IDP', 'BOTH');
         }
         else
         {
-           $types= array('\'IDP\'','\'SP\'','\'BOTH\'');
+            $types = array('IDP', 'SP', 'BOTH');
         }
         $this->providers = new \Doctrine\Common\Collections\ArrayCollection();
         $federations = $provider->getFederations();
         $feds = array();
-        foreach($federations as $f)
+        foreach ($federations as $f)
         {
-            if($f->getActive() === true)
+            if ($f->getActive() === true)
             {
-               $feds[] = $f->getId();
+                $feds[] = $f->getId();
             }
         }
-        if(count($feds) > 0)
+        if (count($feds) > 0)
         {
-          $fedin = implode(',', $feds); 
-          $typesin = implode(',', $types); 
-          $dql = "SELECT p,a FROM models\Provider p LEFT JOIN p.membership a WHERE p.type IN (".$typesin.") AND a.federation IN (".$fedin.") AND a.joinstate != '2' AND a.isBanned='0' AND a.isDisabled = '0' ORDER BY p.name ASC ";
-          $query = $this->em->createQuery($dql);
-          $query->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
-          return $query->getResult();
+
+            $dql = "SELECT p,a FROM models\Provider p LEFT JOIN p.membership a WHERE p.type IN (:types) AND a.federation IN (:feds) AND a.joinstate != '2' AND a.isBanned='0' AND a.isDisabled = '0' ORDER BY p.name ASC ";
+            $query->setParameter('types', $types);
+            $query->setParameter('feds', $feds);
+            $query = $this->em->createQuery($dql);
+            $query->setHint(\Doctrine\ORM\Query::HINT_FORCE_PARTIAL_LOAD, true);
+            return $query->getResult();
         }
-   
+
         return $this->providers;
     }
 
@@ -254,14 +268,18 @@ class Providers
          */
         $federations = $provider->getFederations();
 
-        foreach ($federations->getValues() as $f) {
-            if ($f->getActive()) {
+        foreach ($federations->getValues() as $f)
+        {
+            if ($f->getActive())
+            {
 
                 $y = $f->getMembers();
-                foreach ($y->getKeys() as $key) {
+                foreach ($y->getKeys() as $key)
+                {
                     $type = $y->get($key)->getType();
                     //  log_message('debug','TYPE::::: '.$type);
-                    if ($type == 'SP' OR $type == 'BOTH') {
+                    if ($type == 'SP' OR $type == 'BOTH')
+                    {
                         $this->providers->set($key, $y->get($key));
                     }
                 }
@@ -275,20 +293,25 @@ class Providers
         $this->providers = new \Doctrine\Common\Collections\ArrayCollection();
         $federations = $provider->getFederations();
 
-        foreach ($federations->getValues() as $f) {
-            if ($f->getActive()) {
+        foreach ($federations->getValues() as $f)
+        {
+            if ($f->getActive())
+            {
                 $doFilter['type'] = array('IDP', 'BOTH');
                 $doFilter['local'] = array('1');
-                if (!$onlylocal) {
+                if (!$onlylocal)
+                {
                     $doFilter['local'][] = '0';
                 }
 
                 $y = $f->getMembers()->filter(
-                        function($entry) use($doFilter) {
+                        function($entry) use($doFilter)
+                {
                     return (in_array($entry->getType(), $doFilter['type']) && in_array($entry->getLocal(), $doFilter['local']));
                 }
                 );
-                foreach ($y->getKeys() as $key) {
+                foreach ($y->getKeys() as $key)
+                {
                     //  $type = $y->get($key)->getType();
                     //  if (strcmp($type,'SP')!=0)
                     //  {
@@ -371,10 +394,12 @@ class Providers
         $rsm->addFieldResult('u', 'validto', 'validto');
         $rsm->addFieldResult('u', 'displayname', 'displayname');
         $rsm->addFieldResult('u', 'contacts', 'contacts');
-        if (!empty($local) && $local === TRUE) {
+        if (!empty($local) && $local === TRUE)
+        {
             $query = $this->em->createNativeQuery('SELECT id,name,entityid,helpdeskurl,displayname,is_active,is_approved,validfrom,validto FROM provider WHERE type IN (?,?) AND is_local = \'1\' ORDER BY name ASC', $rsm);
         }
-        else {
+        else
+        {
             $query = $this->em->createNativeQuery('SELECT id,name,entityid,helpdeskurl,displayname,is_active,is_approved,validfrom,validto FROM provider WHERE type IN (?,?) ORDER BY name ASC', $rsm);
         }
         $query->setParameter(1, 'IDP');
@@ -424,12 +449,14 @@ class Providers
         $rsm->addFieldResult('u', 'validfrom', 'validfrom');
         $rsm->addFieldResult('u', 'validto', 'validto');
         $rsm->addFieldResult('u', 'contacts', 'contacts');
-        if (!empty($local) && $local === TRUE) {
+        if (!empty($local) && $local === TRUE)
+        {
             $query = $this->em->createNativeQuery('SELECT id,name,entityid,helpdeskurl,displayname,is_active,is_approved,validfrom,validto FROM provider WHERE type IN (?,?) AND is_local = \'1\' ORDER BY name ASC', $rsm);
             $query->setParameter(1, 'SP');
             $query->setParameter(2, 'BOTH');
         }
-        else {
+        else
+        {
             $query = $this->em->createNativeQuery('SELECT id,name,entityid,helpdeskurl,displayname,is_active,is_approved,validfrom,validto FROM provider WHERE type IN (?,?) ORDER BY name ASC', $rsm);
             $query->setParameter(1, 'SP');
             $query->setParameter(2, 'BOTH');
