a06e7f6e69f4857287dbfcbf5e6cd25dd79836ab
janul <janusz.ulanowski@heanet.ie>
2014-10-06 23:04:57 +0100

    cleaning

--- application/models/AttributeReleasePolicies.php
+++ application/models/AttributeReleasePolicies.php
@@ -1,6 +1,7 @@
 <?php
+
 namespace models;
-use \Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * ResourceRegistry3
  * 
@@ -18,10 +19,8 @@ use \Doctrine\Common\Collections\ArrayCollection;
  * @subpackage  Models
  * @author      Janusz Ulanowski <janusz.ulanowski@heanet.ie>
  */
-
-
-
-class AttributeReleasePolicies {
+class AttributeReleasePolicies
+{
 
     function __construct()
     {
@@ -31,8 +30,8 @@ class AttributeReleasePolicies {
 
     public function getAllPolicies(Provider $idp)
     {
-         $collection = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array('idp' => $idp->getId()));
-         return $collection;
+        $collection = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array('idp' => $idp->getId()));
+        return $collection;
     }
 
     public function getCustomSpPolicyAttributes(Provider $idp, $requester = null)
@@ -45,117 +44,112 @@ class AttributeReleasePolicies {
         }
         return $collection;
     }
+
     public function getCustomSpPolicyAttributesRequester(Provider $requester)
     {
         $collection = array();
         $collection = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array(
-               'requester'=>$requester->getId(),
-               'type'=> 'customsp'));
+            'requester' => $requester->getId(),
+            'type' => 'customsp'));
         return $collection;
     }
 
     public function getSpecArpsToRemoveIDP(Provider $idp)
     {
-          $collection = array();
-          $custom = $this->getCustomSpPolicyAttributes($idp);
-          $specific = $this->getSpecificPolicyAttributes($idp); 
-          $tmp_providers = new Providers;
-          $circle = $tmp_providers->getCircleMembersSP($idp);
-          $circle_ids = array();
-          if(!empty($circle))
-          {
-              foreach($circle as $c)
-              {
-                   $circle_ids[] = $c->getId();
-              }
-          }
-          if(!empty($custom) && is_array($custom) && count($custom) > 0)
-          {
-              foreach($custom as $k)
-              {
-                  if(!in_array($k->getRequester(),$circle_ids))
-                  {
-                       $collection[$k->getId()] = $k;
-                  }
-              }
-          }
-          if(!empty($specific) && is_array($specific) && count($specific) > 0)
-          {
-              foreach($specific as $l)
-              {
-                 if(!in_array($l->getRequester(),$circle_ids))
-                 {
-                      $collection[$l->getId()] = $l;
-                 }
-              }
-          }
+        $collection = array();
+        $custom = $this->getCustomSpPolicyAttributes($idp);
+        $specific = $this->getSpecificPolicyAttributes($idp);
+        $tmp_providers = new Providers;
+        $circle = $tmp_providers->getCircleMembersSP($idp);
+        $circle_ids = array();
+        if (!empty($circle))
+        {
+            foreach ($circle as $c)
+            {
+                $circle_ids[] = $c->getId();
+            }
+        }
+        if (!empty($custom) && is_array($custom) && count($custom) > 0)
+        {
+            foreach ($custom as $k)
+            {
+                if (!in_array($k->getRequester(), $circle_ids))
+                {
+                    $collection[$k->getId()] = $k;
+                }
+            }
+        }
+        if (!empty($specific) && is_array($specific) && count($specific) > 0)
+        {
+            foreach ($specific as $l)
+            {
+                if (!in_array($l->getRequester(), $circle_ids))
+                {
+                    $collection[$l->getId()] = $l;
+                }
+            }
+        }
 
-          return $collection;
-       
+        return $collection;
     }
+
     public function getSpecArpsToRemoveSP(Provider $sp)
     {
-         $collection = array();
-         $custom = $this->getCustomSpPolicyAttributesRequester($sp); 
-         $specific = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array(
-              'type'=>'sp',
-              'requester'=>$sp->getId()));
-         $tmp_providers = new Providers;
-         $circle = $tmp_providers->getCircleMembersIDP($sp);
-         $circle_ids = array();
-         if(!empty($circle))
-         {
-              foreach($circle as $c)
-              {
-                   $circle_ids[] = $c->getId();
-              }
-         }
-         if(!empty($custom) && is_array($custom) && count($custom) > 0)
-         {
-              foreach($custom as $k)
-              {
-                  if(!in_array($k->getProvider()->getId(),$circle_ids))
-                  {
-                       $collection[$k->getId()] = $k;
-                  }
-              }
-         }
-         if(!empty($specific) && is_array($specific) && count($specific) > 0)
-         {
-              foreach($specific as $l)
-              {
-                 if(!in_array($l->getProvider()->getId(),$circle_ids))
-                 {
-                      $collection[$l->getId()] = $l;
-                 }
-              }
-          }
-          return $collection;
-         
-
-         
+        $collection = array();
+        $custom = $this->getCustomSpPolicyAttributesRequester($sp);
+        $specific = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array(
+            'type' => 'sp',
+            'requester' => $sp->getId()));
+        $tmp_providers = new Providers;
+        $circle = $tmp_providers->getCircleMembersIDP($sp);
+        $circle_ids = array();
+        if (!empty($circle))
+        {
+            foreach ($circle as $c)
+            {
+                $circle_ids[] = $c->getId();
+            }
+        }
+        if (!empty($custom) && is_array($custom) && count($custom) > 0)
+        {
+            foreach ($custom as $k)
+            {
+                if (!in_array($k->getProvider()->getId(), $circle_ids))
+                {
+                    $collection[$k->getId()] = $k;
+                }
+            }
+        }
+        if (!empty($specific) && is_array($specific) && count($specific) > 0)
+        {
+            foreach ($specific as $l)
+            {
+                if (!in_array($l->getProvider()->getId(), $circle_ids))
+                {
+                    $collection[$l->getId()] = $l;
+                }
+            }
+        }
+        return $collection;
     }
-   
+
     public function getSpecCustomArpsToRemove(Provider $provider)
     {
-         log_message('debug','getSpecCustomArpsToRemove started for provider:'.$provider->getName());
-         $idparps = array();
-         $sparps = array();
-         $type = $provider->getType();
-         if(strcasecmp($type,'SP')!=0)
-         {
-             $idparps = $this->getSpecArpsToRemoveIDP($provider);
-         }
-         if(strcasecmp($type,'IDP')!=0)
-         {
-             $sparps = $this->getSpecArpsToRemoveSP($provider);
-         }
+        log_message('debug', 'getSpecCustomArpsToRemove started for provider:' . $provider->getName());
+        $idparps = array();
+        $sparps = array();
+        $type = $provider->getType();
+        if (strcasecmp($type, 'SP') != 0)
+        {
+            $idparps = $this->getSpecArpsToRemoveIDP($provider);
+        }
+        if (strcasecmp($type, 'IDP') != 0)
+        {
+            $sparps = $this->getSpecArpsToRemoveSP($provider);
+        }
 
-         $result = array_merge($idparps,$sparps);
-         return $result;
-      
-         
-
+        $result = array_merge($idparps, $sparps);
+        return $result;
     }
 
     public function getSpecificPolicyAttributes(Provider $idp, $requester = null)
@@ -202,7 +196,6 @@ class AttributeReleasePolicies {
 
     public function getFedPolicyAttributesByFed(Provider $idp, Federation $fed)
     {
-       $collection = array();
         $collection = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array(
             'idp' => $idp->getId(),
             'type' => 'fed',
@@ -217,7 +210,7 @@ class AttributeReleasePolicies {
             'type' => 'fed',
             'requester' => $fed->getId(),
             'attribute' => $attrid
-                ));
+        ));
         return $policy;
     }
 
@@ -227,7 +220,7 @@ class AttributeReleasePolicies {
             'idp' => $idpid,
             'attribute' => $attrid,
             'type' => 'global'
-                ));
+        ));
 
         return $policy;
     }
@@ -239,7 +232,7 @@ class AttributeReleasePolicies {
             'attribute' => $attrid,
             'type' => 'fed',
             'requester' => $requester,
-                ));
+        ));
         return $policy;
     }
 
@@ -250,9 +243,10 @@ class AttributeReleasePolicies {
             'attribute' => $attrid,
             'requester' => $requester,
             'type' => 'sp',
-                ));
+        ));
         return $policy;
     }
+
     public function getOneSPCustomPolicy($idpid, $attrid, $requester)
     {
         $policy = $this->em->getRepository("models\AttributeReleasePolicy")->findOneBy(array(
@@ -260,7 +254,7 @@ class AttributeReleasePolicies {
             'attribute' => $attrid,
             'requester' => $requester,
             'type' => 'customsp',
-                ));
+        ));
         return $policy;
     }
 
@@ -272,15 +266,7 @@ class AttributeReleasePolicies {
         $policies = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array(
             'idp' => $idpid,
             'type' => 'sp'));
-        $y = count($policies);
         return $policies;
-    }
-
-    public function getBetaSPPolicy($idpid)
-    {
-        $policies = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array(
-            'idp' => $idpid,
-            'type' => 'sp'));
     }
 
     public function getCustomSpArp($idp, $sp)
@@ -298,8 +284,8 @@ class AttributeReleasePolicies {
             'idp' => $idp->getId(),
             'type' => 'customsp',
             'requester' => $sp->getId(),
-            'attribute'=>$attr->getId()
-            ));
+            'attribute' => $attr->getId()
+        ));
         return $arp;
     }
 
@@ -308,7 +294,7 @@ class AttributeReleasePolicies {
         $collection = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array(
             'idp' => $idp->getId(),
             'attribute' => $attribute->getId()
-                ));
+        ));
         if (!empty($collection))
         {
             foreach ($collection as $c)
@@ -317,20 +303,5 @@ class AttributeReleasePolicies {
             }
         }
     }
-
-    public function getTest(Provider $idp)
-    {
-        $tmp = $this->em->getRepository("models\AttributeReleasePolicy")->findBy(array('idp' => $idp->getId()));
-        if (empty($tmp))
-        {
-            return NULL;
-        }
-        $policy = array();
-
-        foreach ($policy as $p)
-        {
-            
-        }
-    }
-
 }
+
