927a21bfba3336796c48c8caf8b7804cb847eb49
janul <janusz.ulanowski@heanet.ie>
2014-11-28 12:55:44 +0000

    enteditform disable lang values in dropdown if already input field exists

--- js/locals-v7.js
+++ js/locals-v7.js
@@ -807,6 +807,18 @@ $(document).ready(function () {
     var providerEditForm = $('#providereditform');
     if (providerEditForm.length)
     {
+        var langinputrmval ;
+        providerEditForm.find('div.group').each(function(){
+            var selectInside = $(this).find('select').first();
+             $(this).find('button.langinputrm').each(function(){
+                   langinputrmval = $(this).attr('value');
+                   selectInside.find("option[value="+langinputrmval+"]").each(function(){
+                       $(this).toggleOption(true);
+                       $(this).attr('disabled', true);
+                  });
+             });
+        });
+        
         var createRowWithLangRm = function (langCode, langString, inputName, rmbtn) {
             var result = $('<div class=\"large-12 small-12 columns\"><div class=\"small-3 columns\"><label for=\"' + inputName + '\" class=\"right inline\">' + langString + '</label></div><div class=\"small-6 large-7 columns\"><input id=\"' + inputName + '\" name=\"' + inputName + '\" type=\"text\" class=\"validurl\"/></div><div class=\"small-3 large-2 columns\"> <button type=\"button\" class=\"btn langinputrm button inline tiny left alert\" name=\"langrm\" value=\"' + langCode + '\">' + rmbtn + '</button></div></div>');
             return result;
