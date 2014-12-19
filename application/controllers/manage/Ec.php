589888c98ea3d352e9f125c985a7bc8e1d737472
janul <janusz.ulanowski@heanet.ie>
2014-12-19 10:49:13 +0000

    ec can be removed

--- application/controllers/manage/Ec.php
+++ application/controllers/manage/Ec.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 /**
@@ -17,16 +18,15 @@ if (!defined('BASEPATH'))
  * @package     RR3
  * @author      Janusz Ulanowski <janusz.ulanowski@heanet.ie>
  */
-
-class Ec extends MY_Controller
-{
+class Ec extends MY_Controller {
 
     function __construct()
     {
         parent::__construct();
         $loggedin = $this->j_auth->logged_in();
         $this->current_site = current_url();
-        if (!$loggedin) {
+        if (!$loggedin)
+        {
             $this->session->set_flashdata('target', $this->current_site);
             redirect('auth/login', 'location');
         }
@@ -34,131 +34,135 @@ class Ec extends MY_Controller
         $this->load->library('form_validation');
         $this->load->library('zacl');
     }
-      
-    public function show($id=null)
+
+    public function show($id = null)
     {
-       $this->title=lang('title_entcats');
-       if(isset($id))
-       {
-           show_error('Argument passed to page  not allowed',403);
-           return;
-     
-       }
-       $has_write_access = $this->zacl->check_acl('coc', 'write', 'default', '');
-       $obj_list = $this->em->getRepository("models\Coc")->findBy( array('type'=>'entcat'));
-       $data['rows'] = array(); 
-       if(is_array($obj_list) && count($obj_list)>0)
-       {
-          foreach($obj_list as $c)
-          {
-              if($has_write_access)
-              {
-                 $l = '<a href="'.base_url().'manage/ec/edit/'.$c->getId().'" class="button tiny">'.lang('rr_edit').'</a>';
-              }
-              else
-              {
-                 $l = '';
-              }
-              $en = $c->getAvailable();
-              if($en)
-              {
-                  $lbl = '<span class="lbl lbl-active">'.lang('rr_enabled').'</span>';
-              }
-              else
-              {
-                  $lbl = '<span class="lbl lbl-disabled">'.lang('rr_disabled').'</span>';
-              }
-              $subtype = $c->getSubtype();
-              if(empty($subtype))
-              {
-                $subtype = '<span class="label alert">'.lang('lbl_missing').'</span>';
-              }
-              $data['rows'][] = array($c->getName() ,$subtype,anchor($c->getUrl(),$c->getUrl(),array('target' => '_blank', 'class' => 'new_window')), $c->getDescription(),$lbl,$l);
-         
-          } 
-       }
-       else
-       {
-          $data['error_message'] = lang('rr_noentcatsregistered');
-       }
-       $data['showaddbutton'] = FALSE;
-       if($has_write_access)
-       {
-         $data['showaddbutton'] = TRUE;
-       }
+        $this->title = lang('title_entcats');
+        if (isset($id))
+        {
+            show_error('Argument passed to page  not allowed', 403);
+            return;
+        }
+        $has_write_access = $this->zacl->check_acl('coc', 'write', 'default', '');
+        $obj_list = $this->em->getRepository("models\Coc")->findBy(array('type' => 'entcat'));
+        $data['rows'] = array();
+        if (is_array($obj_list) && count($obj_list) > 0)
+        {
+            foreach ($obj_list as $c)
+            {
+                $isEnabled = $c->getAvailable();
+                if ($has_write_access)
+                {
+                    $l = '<a href="' . base_url() . 'manage/ec/edit/' . $c->getId() . '" ><i class="fi-pencil"></i></a>';
+                    if (!$isEnabled)
+                    {
+                        $l .= '&nbsp;&nbsp;<a href="' . base_url() . 'manage/ec/remove/' . $c->getId() . '" class="withconfirm" data-jagger-ec="' . $c->getId() . '"><i class="fi-trash"></i></a>';
+                    }
+                }
+                else
+                {
+                    $l = '';
+                }
 
-       $data['titlepage'] = lang('ent_list_title');
+                if ($isEnabled)
+                {
+                    $lbl = '<span class="lbl lbl-active">' . lang('rr_enabled') . '</span>';
+                }
+                else
+                {
+                    $lbl = '<span class="lbl lbl-disabled">' . lang('rr_disabled') . '</span>';
+                }
+                $subtype = $c->getSubtype();
+                if (empty($subtype))
+                {
+                    $subtype = '<span class="label alert">' . lang('lbl_missing') . '</span>';
+                }
+                $data['rows'][] = array($c->getName(), $subtype, anchor($c->getUrl(), $c->getUrl(), array('target' => '_blank', 'class' => 'new_window')), $c->getDescription(), $lbl, $l);
+            }
+        }
+        else
+        {
+            $data['error_message'] = lang('rr_noentcatsregistered');
+        }
+        $data['showaddbutton'] = FALSE;
+        if ($has_write_access)
+        {
+            $data['showaddbutton'] = TRUE;
+        }
 
-       $data['content_view'] = 'manage/coc_show_view';
-       $this->load->view('page',$data);
-          
+        $data['titlepage'] = lang('ent_list_title');
+
+        $data['content_view'] = 'manage/coc_show_view';
+        $this->load->view('page', $data);
     }
+
     private function _add_submit_validate()
     {
-        $this->form_validation->set_rules('name',lang('entcat_displayname'),'required|trim|cocname_unique');
-        $this->form_validation->set_rules('attrname',lang('rr_attr_name'),'required|trim|xss_clean');
+        $this->form_validation->set_rules('name', lang('entcat_displayname'), 'required|trim|cocname_unique');
+        $this->form_validation->set_rules('attrname', lang('rr_attr_name'), 'required|trim|xss_clean');
         $attrname = $this->input->post('attrname');
-        $this->form_validation->set_rules('url',lang('entcat_value'),'required|trim|valid_url|ecUrlInsert['.$attrname.']');
-        $this->form_validation->set_rules('description',lang('entcat_description'),'xss_clean');
-        $this->form_validation->set_rules('cenabled',lang('entcat_enabled'),'xss_clean');
+        $this->form_validation->set_rules('url', lang('entcat_value'), 'required|trim|valid_url|ecUrlInsert[' . $attrname . ']');
+        $this->form_validation->set_rules('description', lang('entcat_description'), 'xss_clean');
+        $this->form_validation->set_rules('cenabled', lang('entcat_enabled'), 'xss_clean');
         return $this->form_validation->run();
     }
+
     private function _edit_submit_validate($id)
     {
-        $attrname = $this->input->post('attrname');    
-        $this->form_validation->set_rules('name',lang('entcat_displayname'),'required|trim|cocname_unique_update['.$id.']');
-        $this->form_validation->set_rules('attrname',lang('rr_attr_name'),'required|trim');
-        $ecUrlUpdateParams = serialize(array('id'=>$id,'subtype'=>$attrname));
-        $this->form_validation->set_rules('url',lang('entcat_value'),'required|trim|valid_url|ecUrlUpdate['.$ecUrlUpdateParams.']');
-        $this->form_validation->set_rules('description',lang('entcat_description'),'xss_clean');
-        $this->form_validation->set_rules('cenabled',lang('entcat_enabled'),'xss_clean');
+        $attrname = $this->input->post('attrname');
+        $this->form_validation->set_rules('name', lang('entcat_displayname'), 'required|trim|cocname_unique_update[' . $id . ']');
+        $this->form_validation->set_rules('attrname', lang('rr_attr_name'), 'required|trim');
+        $ecUrlUpdateParams = serialize(array('id' => $id, 'subtype' => $attrname));
+        $this->form_validation->set_rules('url', lang('entcat_value'), 'required|trim|valid_url|ecUrlUpdate[' . $ecUrlUpdateParams . ']');
+        $this->form_validation->set_rules('description', lang('entcat_description'), 'xss_clean');
+        $this->form_validation->set_rules('cenabled', lang('entcat_enabled'), 'xss_clean');
         return $this->form_validation->run();
     }
+
     public function add()
     {
         $this->title = lang('title_addentcat');
-        $data['titlepage'] =  lang('title_addentcat');
+        $data['titlepage'] = lang('title_addentcat');
         $has_write_access = $this->zacl->check_acl('coc', 'write', 'default', '');
-        if(!$has_write_access)
+        if (!$has_write_access)
         {
-            show_error('No access',401);
+            show_error('No access', 401);
             return;
         }
 
-        if($this->_add_submit_validate() === TRUE)
+        if ($this->_add_submit_validate() === TRUE)
         {
-           $name = $this->input->post('name');
-           $url = $this->input->post('url');
-           $cenabled = $this->input->post('cenabled');
-           $description = $this->input->post('description');
-          
-           $ncoc = new models\Coc;
-           $ncoc->setName($name);
-           $ncoc->setUrl($url);
-           $ncoc->setType('entcat');
-           $allowedattrs = attrsEntCategoryList();
-           $inputAttrname = $this->input->post('attrname');
-           if(in_array($inputAttrname,$allowedattrs))
-           { 
-              $ncoc->setSubtype($inputAttrname);
+            $name = $this->input->post('name');
+            $url = $this->input->post('url');
+            $cenabled = $this->input->post('cenabled');
+            $description = $this->input->post('description');
 
-           }
-           if(!empty($description))
-           {
-               $ncoc->setDescription($description);
-           }
-           if(!empty($cenabled) && $cenabled == 'accept')
-           {
-              $ncoc->setAvailable(TRUE);
-           }
-           else
-           {
-              $ncoc->setAvailable(FALSE);
-           }
-           $this->em->persist($ncoc);
-           $this->em->flush();
-           
-          $data['success_message'] = lang('rr_entcatadded');
+            $ncoc = new models\Coc;
+            $ncoc->setName($name);
+            $ncoc->setUrl($url);
+            $ncoc->setType('entcat');
+            $allowedattrs = attrsEntCategoryList();
+            $inputAttrname = $this->input->post('attrname');
+            if (in_array($inputAttrname, $allowedattrs))
+            {
+                $ncoc->setSubtype($inputAttrname);
+            }
+            if (!empty($description))
+            {
+                $ncoc->setDescription($description);
+            }
+            if (!empty($cenabled) && $cenabled == 'accept')
+            {
+                $ncoc->setAvailable(TRUE);
+            }
+            else
+            {
+                $ncoc->setAvailable(FALSE);
+            }
+            $this->em->persist($ncoc);
+            $this->em->flush();
+
+            $data['success_message'] = lang('rr_entcatadded');
         }
         else
         {
@@ -166,75 +170,115 @@ class Ec extends MY_Controller
             $this->load->library('form_element');
             $f .= $this->form_element->generateAddCoc();
             $f .= '<div class="buttons small-12 medium-10 large-10 columns end text-right">';
-            $f .= '<button type="reset" name="reset" value="reset" class="resetbutton reseticon alert">'.lang('rr_reset').'</button> ';
-            $f .= '<button type="submit" name="modify" value="submit" class="savebutton saveicon">'.lang('rr_save').'</button></div>';
+            $f .= '<button type="reset" name="reset" value="reset" class="resetbutton reseticon alert">' . lang('rr_reset') . '</button> ';
+            $f .= '<button type="submit" name="modify" value="submit" class="savebutton saveicon">' . lang('rr_save') . '</button></div>';
 
             $f .= form_close();
             $data['form'] = $f;
         }
-            $data['content_view'] = 'manage/coc_add_view';
-            $this->load->view('page',$data);
+        $data['content_view'] = 'manage/coc_add_view';
+        $this->load->view('page', $data);
     }
+
     public function edit($id)
     {
-       $this->title = lang('title_entcatedit');
-      
-       if(empty($id) || !is_numeric($id))
-       {
-          show_error('Not found',404);
-          return;
-       }
-       $coc = $this->em->getRepository("models\Coc")->findOneBy(array('id'=>$id,'type'=>'entcat'));
-       if(empty($coc))
-       {
-          show_error('Not found',404);
-          return;
-       }
-       $has_write_access = $this->zacl->check_acl('coc', 'write', 'default', '');
-       if(!$has_write_access)
-       {
-           show_error('No access',401);
-           return;
-       }
-       $data['titlepage'] = lang('title_entcat').': '.htmlentities($coc->getName());
-       $data['subtitlepage'] = lang('title_entcatedit');
+        $this->title = lang('title_entcatedit');
 
-       if($this->_edit_submit_validate($id) === TRUE)
-       {
-           $enable = $this->input->post('cenabled');
-           if(!empty($enable) && $enable == 'accept')
-           {
-               $coc->setAvailable(TRUE);
-           }
-           else
-           {
-               $coc->setAvailable(FALSE);
-           }
-           $coc->setName($this->input->post('name'));
-           $coc->setUrl($this->input->post('url'));
-           $allowedattrs = attrsEntCategoryList();
-           $inputAttrname = $this->input->post('attrname');
-           if(in_array($inputAttrname,$allowedattrs))
-           { 
-              $coc->setSubtype($inputAttrname);
-           }
-           $coc->setDescription($this->input->post('description'));
-           $this->em->persist($coc);
-           $this->em->flush();
-           $data['success_message'] = lang('updated');
-       }
-       $data['coc_name'] = $coc->getName();
-       $this->load->library('form_element');
-       $f = form_open();
-       $f .= $this->form_element->generateEditCoc($coc);
-       $f .= '<div class="buttons large-10 medium-10 small-12 text-right columns end">';
-       $f .= '<button type="reset" name="reset" value="reset" class="resetbutton reseticon alert">'.lang('rr_reset').'</button> ';
-       $f .= '<button type="submit" name="modify" value="submit" class="savebutton saveicon">'.lang('rr_save').'</button></div>';
-       $f .= form_close();
-       $data['form'] = $f;
-       $data['content_view'] = 'manage/coc_edit_view';
-       $this->load->view('page',$data);
-       
+        if (empty($id) || !is_numeric($id))
+        {
+            show_error('Not found', 404);
+            return;
+        }
+        $coc = $this->em->getRepository("models\Coc")->findOneBy(array('id' => $id, 'type' => 'entcat'));
+        if (empty($coc))
+        {
+            show_error('Not found', 404);
+            return;
+        }
+        $has_write_access = $this->zacl->check_acl('coc', 'write', 'default', '');
+        if (!$has_write_access)
+        {
+            show_error('No access', 401);
+            return;
+        }
+        $data['titlepage'] = lang('title_entcat') . ': ' . htmlentities($coc->getName());
+        $data['subtitlepage'] = lang('title_entcatedit');
+
+        if ($this->_edit_submit_validate($id) === TRUE)
+        {
+            $enable = $this->input->post('cenabled');
+            if (!empty($enable) && $enable == 'accept')
+            {
+                $coc->setAvailable(TRUE);
+            }
+            else
+            {
+                $coc->setAvailable(FALSE);
+            }
+            $coc->setName($this->input->post('name'));
+            $coc->setUrl($this->input->post('url'));
+            $allowedattrs = attrsEntCategoryList();
+            $inputAttrname = $this->input->post('attrname');
+            if (in_array($inputAttrname, $allowedattrs))
+            {
+                $coc->setSubtype($inputAttrname);
+            }
+            $coc->setDescription($this->input->post('description'));
+            $this->em->persist($coc);
+            $this->em->flush();
+            $data['success_message'] = lang('updated');
+        }
+        $data['coc_name'] = $coc->getName();
+        $this->load->library('form_element');
+        $f = form_open();
+        $f .= $this->form_element->generateEditCoc($coc);
+        $f .= '<div class="buttons large-10 medium-10 small-12 text-right columns end">';
+        $f .= '<button type="reset" name="reset" value="reset" class="resetbutton reseticon alert">' . lang('rr_reset') . '</button> ';
+        $f .= '<button type="submit" name="modify" value="submit" class="savebutton saveicon">' . lang('rr_save') . '</button></div>';
+        $f .= form_close();
+        $data['form'] = $f;
+        $data['content_view'] = 'manage/coc_edit_view';
+        $this->load->view('page', $data);
     }
-    
+
+     function remove($id=null)
+    {
+        if(empty($id) || !ctype_digit($id))
+        {
+            set_status_header(404);
+            echo 'incorrect id or id not provided';
+            return;
+        }
+        if(!$this->input->is_ajax_request())
+        {
+            set_status_header(403);
+            echo 'access denied';
+            return;
+        }
+        $loggedin = $this->j_auth->logged_in();
+        if(!$loggedin)
+        {
+            set_status_header(403);
+            echo 'access denied';
+            return;
+        }
+        $has_write_access = $this->zacl->check_acl('coc', 'write', 'default', '');
+        if(!$has_write_access)
+        {
+            set_status_header(403);
+            echo 'access denied';
+            return;
+        }
+        $ec = $this->em->getRepository("models\Coc")->findOneBy( array('id'=>''.$id.'','type'=>'entcat','is_enabled'=>false));
+        if(empty($ec))
+        {
+            set_status_header(403);
+            echo 'Registration policy doesnt exist or is not disabled';
+            return;
+        }
+        $this->em->remove($ec);
+        $this->em->flush();
+        echo "OK";
+        return;
+    }
 }
