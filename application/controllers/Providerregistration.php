f3c7c8930d4f9871918a94e09c90676ee21bc2c5
janul <janusz.ulanowski@heanet.ie>
2015-03-11 11:20:24 +0000

    cleaning registrationforms

---
+++ application/controllers/Providerregistration.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Providerregistration extends MY_Controller{
+
+     function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('form');
+        MY_Controller::$menuactive = 'reg';
+
+    }
+    function idp()
+    {
+
+    }
+    function sp()
+    {
+
+        $this->title = lang('title_spreg');
+        $data['titlepage'] = lang('rr_sp_register_title');
+        $data['content_view'] = 'sp/sp_registration_form_view';
+        $data['breadcrumbs'] = array(
+            array('url'=>'#','name'=>lang('rr_sp_register_title'),'type'=>'current'),
+
+        );
+        $this->load->view('page', $data);
+    }
+}
