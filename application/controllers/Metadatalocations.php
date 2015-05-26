d8ee59b632c8f733425f8b404b12537bd9463a1b
janul <janusz.ulanowski@heanet.ie>
2015-05-26 09:46:43 +0100

    cleaning

--- application/controllers/Metadatalocations.php
+++ application/controllers/Metadatalocations.php
@@ -27,35 +27,34 @@ class Metadatalocations extends MY_Controller {
        $this->load->library('table');
 
         $myLang = MY_Controller::getLang();
-        $providerprefix = base_url() . "signedmetadata/provider/"; 
-        $federationprefix = base_url() . "signedmetadata/federation/";
-        $tmp_providers = new models\Providers;
-        $tmp_federations = new models\Federations;
-        $onlylocals = TRUE;
-        $feds = $tmp_federations->getFederations();
+        $providerPrefixUrl = base_url('signedmetadata/provider') ;
+        $federationPrefixUrl = base_url('signedmetadata/federation') ;
+        $tmpProviders = new models\Providers;
+        $tmpFederations = new models\Federations;
+        $feds = $tmpFederations->getFederations();
 
         /**
          * @var $sps models\Provider[]
          * @var $idps models\Provider[]
          */
-        $sps = $tmp_providers->getPublicSps_inNative();
-        $idps = $tmp_providers->getPublicIdps_inNative();
+        $sps = $tmpProviders->getPublicSps_inNative();
+        $idps = $tmpProviders->getPublicIdps_inNative();
         $farray = array();
         foreach($feds as $fed)
         {
-            $farray[] = array('<a href="'.$federationprefix. $fed->getSysname().'/metadata.xml">signed metadata</a>', 
+            $farray[] = array('<a href="'.$federationPrefixUrl.'/'. $fed->getSysname().'/metadata.xml">signed metadata</a>',
                               '<span title="'.$fed->getName().'">'.$fed->getName().'</span>',$fed->getUrn());
         }
         $tarray = array();
         foreach($idps as $idp)
         {
-            $tarray[] = array('<a href="'.$providerprefix. base64url_encode($idp->getEntityId()).'/metadata.xml">signed metadata</a>', '<span title="'.$idp->getNameToWebInLang($myLang,'idp').'">'.$idp->getDisplayName(40).'</span>',$idp->getEntityId());
+            $tarray[] = array('<a href="'.$providerPrefixUrl.'/'. base64url_encode($idp->getEntityId()).'/metadata.xml">signed metadata</a>', '<span title="'.$idp->getNameToWebInLang($myLang,'idp').'">'.$idp->getDisplayName(40).'</span>',$idp->getEntityId());
         }
 
         $sarray = array();
         foreach($sps as $sp)
         {
-            $sarray[] = array('<a href="'.$providerprefix. base64url_encode($sp->getEntityId()).'/metadata.xml">signed metadata</a>', '<span title="'.$sp->getNameToWebInLang($myLang,'sp').'">'.$sp->getDisplayName(40).'<span>',$sp->getEntityId());
+            $sarray[] = array('<a href="'.$providerPrefixUrl.'/'. base64url_encode($sp->getEntityId()).'/metadata.xml">signed metadata</a>', '<span title="'.$sp->getNameToWebInLang($myLang,'sp').'">'.$sp->getDisplayName(40).'<span>',$sp->getEntityId());
         }
         $data['farray'] = &$farray;
         $data['tarray'] = &$tarray;
