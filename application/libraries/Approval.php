a4df8fb5449bb3f9a022d44d5864913ee0aba53c
janul <janusz.ulanowski@heanet.ie>
2015-08-27 22:23:37 +0100

    cleaning

--- application/libraries/Approval.php
+++ application/libraries/Approval.php
@@ -1,33 +1,27 @@
 <?php
-if (!defined('BASEPATH'))
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
+}
+
 /**
  * ResourceRegistry3
- * 
- * @package     RR3
- * @author      Middleware Team HEAnet 
- * @copyright   Copyright (c) 2012, HEAnet Limited (http://www.heanet.ie)
- * @license     MIT http://www.opensource.org/licenses/mit-license.php
- *  
+ *
+ * @package   RR3
+ * @author    Middleware Team HEAnet
+ * @author    Janusz Ulanowski <janusz.ulanowski@heanet.ie>
+ * @copyright Copyright (c) 2012, HEAnet Limited (http://www.heanet.ie)
+ * @license   MIT http://www.opensource.org/licenses/mit-license.php
  */
-
-/**
- * Approval Class
- * 
- * @package     RR3
- * @subpackage  Libraries
- * @author      Janusz Ulanowski <janusz.ulanowski@heanet.ie>
- */
-
-
-class Approval {
+class Approval
+{
 
     protected $ci;
     protected $em;
     protected $user;
 
-    function __construct() {
-        $this->ci = & get_instance();
+    public function __construct()
+    {
+        $this->ci = &get_instance();
         $this->em = $this->ci->doctrine->em;
         $this->user = $this->em->getRepository("models\User")->findOneBy(array('username' => $this->ci->session->userdata('username')));
 
@@ -38,13 +32,14 @@ class Approval {
      * @param $action
      * @return \models\Queue
      */
-    public function addToQueue($obj, $action) {
-        log_message('debug',__METHOD__.': obj: '.get_class($obj).' , action: '.$action);
+    public function addToQueue($obj, $action)
+    {
+        log_message('debug', __METHOD__ . ': obj: ' . get_class($obj) . ' , action: ' . $action);
         $queue = new models\Queue();
         if ($obj instanceof models\Federation) {
             $queue->addFederation($obj->convertToArray());
             /**
-             * @todo decide if to verify action value 
+             * @todo decide if to verify action value
              */
             $queue->setAction($action);
             $queue->setName($obj->getName());
@@ -56,26 +51,26 @@ class Approval {
     }
 
     /**
-     * @param \models\Coc $coc
+     * @param \models\Coc      $coc
      * @param \models\Provider $provider
      * @return \models\Queue
      */
     public function applyForEntityCategory(models\Coc $coc, models\Provider $provider)
     {
-          $q = new models\Queue();
-          $q->setRecipient($coc->getId());
-          $q->setRecipientType('entitycategory');
-          $q->setCreator($this->user);
-          $q->setName($provider->getEntityId());
-          $q->setEmail($this->user->getEmail());
-          $q->setConfirm(TRUE);
-          $q->setAction('APPLY');
-          $q->setType('Provider');
-          $q->setObjectType('n');
-          $q->setObject(array());
-          $q->setToken();
-          $this->em->persist($q);
-          return $q;
+        $nQueue = new models\Queue();
+        $nQueue->setRecipient($coc->getId());
+        $nQueue->setRecipientType('entitycategory');
+        $nQueue->setCreator($this->user);
+        $nQueue->setName($provider->getEntityId());
+        $nQueue->setEmail($this->user->getEmail());
+        $nQueue->setConfirm(true);
+        $nQueue->setAction('APPLY');
+        $nQueue->setType('Provider');
+        $nQueue->setObjectType('n');
+        $nQueue->setObject(array());
+        $nQueue->setToken();
+        $this->em->persist($nQueue);
+        return $nQueue;
     }
 
     /**
@@ -85,20 +80,20 @@ class Approval {
      */
     public function applyForRegistrationPolicy(models\Coc $coc, models\Provider $provider)
     {
-          $q = new models\Queue();
-          $q->setRecipient($coc->getId());
-          $q->setRecipientType('regpolicy');
-          $q->setCreator($this->user);
-          $q->setName($provider->getEntityId());
-          $q->setEmail($this->user->getEmail());
-          $q->setConfirm(TRUE);
-          $q->setAction('APPLY');
-          $q->setType('Provider');
-          $q->setObjectType('n');
-          $q->setObject(array());
-          $q->setToken();
-          $this->em->persist($q);
-          return $q;
+        $nQueue = new models\Queue();
+        $nQueue->setRecipient($coc->getId());
+        $nQueue->setRecipientType('regpolicy');
+        $nQueue->setCreator($this->user);
+        $nQueue->setName($provider->getEntityId());
+        $nQueue->setEmail($this->user->getEmail());
+        $nQueue->setConfirm(true);
+        $nQueue->setAction('APPLY');
+        $nQueue->setType('Provider');
+        $nQueue->setObjectType('n');
+        $nQueue->setObject(array());
+        $nQueue->setToken();
+        $this->em->persist($nQueue);
+        return $nQueue;
     }
 
     /**
@@ -107,23 +102,22 @@ class Approval {
      * @param $action
      * @return \models\Queue
      */
-    public function invitationProviderToQueue(models\Federation $federation ,models\Provider $obj,$action)
+    public function invitationProviderToQueue(models\Federation $federation, models\Provider $obj, $action)
     {
-           $queue = new models\Queue();
-           $queue->setRecipient($obj->getId());
-           $queue->setRecipientType('provider');
-	   $queue->setCreator($this->user);
-           $queue->setName($federation->getName());
-           $queue->setEmail($this->user->getEmail());
-           $fed = array('id'=>$federation->getId(), 'name'=>$federation->getName(), 'urn'=>$federation->getUrn());
-           if($action == 'Join')
-           {
-                $queue->inviteProvider($fed);
-           }
-           $queue->setToken();
-           $this->em->persist($queue);
-           $this->em->flush();
-           return $queue;
+        $nQueue = new models\Queue();
+        $nQueue->setRecipient($obj->getId());
+        $nQueue->setRecipientType('provider');
+        $nQueue->setCreator($this->user);
+        $nQueue->setName($federation->getName());
+        $nQueue->setEmail($this->user->getEmail());
+        $fed = array('id' => $federation->getId(), 'name' => $federation->getName(), 'urn' => $federation->getUrn());
+        if ($action === 'Join') {
+            $nQueue->inviteProvider($fed);
+        }
+        $nQueue->setToken();
+        $this->em->persist($nQueue);
+        $this->em->flush();
+        return $nQueue;
     }
 
     /**
@@ -132,23 +126,22 @@ class Approval {
      * @param $action
      * @return \models\Queue
      */
-    public function removeProviderToQueue(models\Federation $federation ,models\Provider $obj,$action)
+    public function removeProviderToQueue(models\Federation $federation, models\Provider $obj, $action)
     {
-           $queue = new models\Queue();
-           $queue->setRecipient($obj->getId());
-           $queue->setRecipientType('provider');
-	   $queue->setCreator($this->user);
-           $queue->setName($federation->getName());
-           $queue->setEmail($this->user->getEmail());
-           $fed = array('id'=>$federation->getId(), 'name'=>$federation->getName(), 'urn'=>$federation->getUrn());
-           if($action == 'Leave')
-           {
-                $queue->leaveProvider($fed);
-           }
-           $queue->setToken();
-           $this->em->persist($queue);
-           $this->em->flush();
-           return $queue;
+        $nQueue = new models\Queue();
+        $nQueue->setRecipient($obj->getId());
+        $nQueue->setRecipientType('provider');
+        $nQueue->setCreator($this->user);
+        $nQueue->setName($federation->getName());
+        $nQueue->setEmail($this->user->getEmail());
+        $fed = array('id' => $federation->getId(), 'name' => $federation->getName(), 'urn' => $federation->getUrn());
+        if ($action === 'Leave') {
+            $nQueue->leaveProvider($fed);
+        }
+        $nQueue->setToken();
+        $this->em->persist($nQueue);
+        $this->em->flush();
+        return $nQueue;
     }
 
     /**
@@ -158,29 +151,27 @@ class Approval {
      * @param null $message
      * @return \models\Queue
      */
-    public function invitationFederationToQueue(models\Provider $provider ,models\Federation $obj,$action,$message = null)
+    public function invitationFederationToQueue(models\Provider $provider, models\Federation $obj, $action, $message = null)
     {
 
-          $queue = new models\Queue();
-          $queue->setRecipient($obj->getId());
-          $queue->setRecipientType('federation');
-          $queue->setCreator($this->user);
-          $providername = $provider->getName();
-          if(empty($providername))
-          {
-             $providername = $provider->getEntityId();
-          }
-          $queue->setName($providername);
-          $queue->setEmail($this->user->getEmail());
-          $prov = array('id'=>$provider->getId(), 'name'=>$providername, 'entityid'=>$provider->getEntityId(),'message'=>''.$message.'');
-          if($action == 'Join')
-          {
-             $queue->inviteFederation($prov);
-          }
-          $queue->setToken();
-          $this->em->persist($queue);
-          $this->em->flush();
-          return $queue;
+        $nQueue = new models\Queue();
+        $nQueue->setRecipient($obj->getId());
+        $nQueue->setRecipientType('federation');
+        $nQueue->setCreator($this->user);
+        $providername = $provider->getName();
+        if (empty($providername)) {
+            $providername = $provider->getEntityId();
+        }
+        $nQueue->setName($providername);
+        $nQueue->setEmail($this->user->getEmail());
+        $prov = array('id' => $provider->getId(), 'name' => $providername, 'entityid' => $provider->getEntityId(), 'message' => '' . $message . '');
+        if ($action === 'Join') {
+            $nQueue->inviteFederation($prov);
+        }
+        $nQueue->setToken();
+        $this->em->persist($nQueue);
+        $this->em->flush();
+        return $nQueue;
     }
 
     /**
@@ -189,14 +180,14 @@ class Approval {
      */
     public function removeFederation(models\Federation $federation)
     {
-         $queue = new models\Queue();
-         $queue->setCreator($this->user);
-         $queue->setName($federation->getName());
-         $queue->setEmail($this->user->getEmail());
-         $queue->setAction('Delete');
-         $queue->addFederation($federation->convertToArray());
-         $queue->setToken();
-         return $queue;
+        $nQueue = new models\Queue();
+        $nQueue->setCreator($this->user);
+        $nQueue->setName($federation->getName());
+        $nQueue->setEmail($this->user->getEmail());
+        $nQueue->setAction('Delete');
+        $nQueue->addFederation($federation->convertToArray());
+        $nQueue->setToken();
+        return $nQueue;
     }
 
 }
