87b815ca4523b5aa19136fbae8ffd6f20d20da63
janul <janusz.ulanowski@heanet.ie>
2015-09-01 22:52:32 +0100

    cont userprofile editform in progress

--- application/controllers/manage/Userprofile.php
+++ application/controllers/manage/Userprofile.php
@@ -29,15 +29,27 @@ class Userprofile extends MY_Controller
     /**
      * @param null $encodedUsername
      */
-    private function edit($encodedUsername = null)
+    public function edit($encodedUsername = null)
     {
+        $grantAccess = false;
+        $loggedinUser = $this->j_auth->current_user();
+        if ($loggedinUser === null) {
+            show_error('ff', 500);
+        }
         if ($encodedUsername === null) {
-            $decodedUsername = $this->session->userdata('username');
+            $decodedUsername = $loggedinUser;
+            $grantAccess = true;
         } else {
             $decodedUsername = base64url_decode($encodedUsername);
+            if ($decodedUsername === $loggedinUser || $this->isAdmin) {
+                $grantAccess = true;
+            }
         }
 
 
+        if ($grantAccess !== true) {
+            show_error('fsdfsd', 403);
+        }
         /**
          * @var models\User $user
          */
@@ -46,7 +58,39 @@ class Userprofile extends MY_Controller
             show_error(404, 'User not found');
         }
 
-        $data['content_view'] = 'manage/userprofile_edit';
+
+
+        $data = array(
+            'username'=>$user->getUsername(),
+            'fname'=> $user->getGivenname(),
+            'sname'=>$user->getSurname(),
+            'email'=>$user->getEmail(),
+            'local'=>$user->getLocal(),
+            'federated'=>(bool) $user->getFederated(),
+            'isadmin' => $this->isAdmin,
+            'show2fa' => false,
+            'content_view'=> 'manage/userprofile_edit',
+        );
+        $systeRoles = array('Administrator','Member','Guest');
+        $userSRolesNames = $user->getSystemRoleNames();
+
+        foreach($systeRoles as $role)
+        {
+            if(in_array($role,$userSRolesNames))
+            {
+                $data['roles'][''.$role.''] = true;
+            }
+            else
+            {
+                $data['roles'][''.$role.''] = false;
+            }
+        }
+        $allowed2fglobal = $this->rrpreference->getStatusByName('user2fset');
+        if ($this->isAdmin  || ($grantAccess && $allowed2fglobal)) {
+            $data['show2fa'] = true;
+        }
+
+
         $this->load->view('page', $data);
 
     }
--- application/views/manage/userprofile_edit.php
+++ application/views/manage/userprofile_edit.php
@@ -5,47 +5,64 @@ echo form_open();
 echo '
 <div class="small-12 column">
     <div class="medium-3 column medium-text-right"><label>' . lang('rr_username') . '</label></div>
-    <div class="medium-6 end column">'.form_input(array('name'=>'username','type'=>'text','readonly'=>'readonly','value'=>html_escape($username))).'</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'username', 'type' => 'text', 'readonly' => 'readonly', 'value' => html_escape($username))) . '</div>
 </div>';
 if (!$isadmin) {
     echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="currentpass">' . lang('rr_oldpassword') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'currentpass','type'=>'password','autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'currentpass', 'type' => 'password', 'autocomplete' => 'off')) . '</div>
 </div>';
 }
-  echo '<div class="small-12 column">
+echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="npassword">' . lang('rr_npassword') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'npassword','type'=>'password','value'=>'','autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'npassword', 'type' => 'password', 'value' => '', 'autocomplete' => 'off')) . '</div>
 </div>';
 echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="confirmnpassword">' . lang('rr_npasswordconf') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'confirmnpassword','type'=>'password','value'=>'','autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'confirmnpassword', 'type' => 'password', 'value' => '', 'autocomplete' => 'off')) . '</div>
 </div>';
 
 echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="fname">' . lang('rr_fname') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'fname','type'=>'text','value'=>set_value('fname',$fname),'autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'fname', 'type' => 'text', 'value' => set_value('fname', $fname), 'autocomplete' => 'off')) . '</div>
 </div>';
 echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="sname">' . lang('rr_lname') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'sname','type'=>'text','value'=>set_value('sname',$sname),'autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'sname', 'type' => 'text', 'value' => set_value('sname', $sname), 'autocomplete' => 'off')) . '</div>
 </div>';
 
 echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="email">' . lang('rr_uemail') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'email','type'=>'text','value'=>set_value('email',$email),'autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'email', 'type' => 'text', 'value' => set_value('email', $email), 'autocomplete' => 'off')) . '</div>
 </div>';
 
 echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="confirmemail">' . lang('rr_confirmuemail') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'confirmemail','type'=>'text','value'=>set_value('confirmemail'),'autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_input(array('name' => 'confirmemail', 'type' => 'text', 'value' => set_value('confirmemail'), 'autocomplete' => 'off')) . '</div>
 </div>';
-if($isadmin)
-{
+if ($isadmin) {
     echo '<div class="small-12 column">
   <div class="medium-3 column medium-text-right"><label for="accesstype">' . lang('rr_typeaccess') . '</label></div>
-    <div class="medium-6 end column">' . form_input(array('name'=>'confirmemail','type'=>'text','value'=>set_value('confirmemail'),'autocomplete'=>'off')) . '</div>
+    <div class="medium-6 end column">' . form_checkbox(array('name' => 'accesstype', 'value' => 'fed', 'checked' => $federated)) . '<label>Federated</label>
+      ' . form_checkbox(array('name' => 'accesstype', 'value' => 'local', 'checked' => $local)) . '<label>Local authentication</label> </div>
 </div>';
+
+    echo '<div class="small-12 column">
+  <div class="medium-3 column medium-text-right"><label for="accessroles">Access Roles</label></div>
+    <div class="medium-6 end column">';
+    foreach ($roles as $key => $value) {
+    echo form_checkbox(array('name' => 'accessroles', 'value' => ''.$key.'', 'checked' => $value)) . '<label>'.$key.'</label>';
+    }
+    echo '</div>
+</div>';
+
+}
+if($show2fa === true)
+{
+  echo '<div class="small-12 column">
+  <div class="medium-3 column medium-text-right"><label for="secondfactor">2ndFactor</label></div>
+    <div class="medium-6 end column">';
+  echo '</div></div>';
 }
 
 echo form_close();
