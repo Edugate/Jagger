cfbe93419ff27421608ce01235a72d311aeeba23
janul <janusz.ulanowski@heanet.ie>
2015-11-17 08:50:51 +0000

    cleaning

--- application/libraries/Providertoxml.php
+++ application/libraries/Providertoxml.php
@@ -1,16 +1,14 @@
 <?php
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 /**
- * Description of Providertoxml
+ * @package   Jagger
+ * @author    Middleware Team HEAnet
+ * @author    Janusz Ulanowski <janusz.ulanowski@heanet.ie>
+ * @copyright 20115 HEAnet Limited (http://www.heanet.ie)
+ * @license   MIT http://www.opensource.org/licenses/mit-license.php
  *
- * @author janul
  */
 class Providertoxml
 {
