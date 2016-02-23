347d02cb29e2bd3477df1627a10a403ecd23c3f9
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2016-02-23 10:36:28 +0000

    cleaning contd

--- application/views/manage/joinfederation_view.php
+++ application/views/manage/joinfederation_view.php
@@ -1,6 +1,7 @@
 <?php
-if (!defined('BASEPATH'))
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
+}
 
 if(!empty($success_message))
 {
@@ -11,10 +12,10 @@ if(!empty($error_message))
     echo '<div  data-alert class="alert-box alert">'.$error_message.'</div>';
 
 }
-echo '<div data-alert class="alert-box warning validaronotice" style="display:none">'.lang('fvalidatorjoinfed').'</div>';
+echo '<div data-alert class="alert-box warning validaronotice hidden">'.lang('fvalidatorjoinfed').'</div>';
 echo '<div class="button-bar"><ul class="button-group validatorbuttons"></ul></div>';
 
-$attrs = array('id'=>'fvform','style'=>'display:none;','class'=>'columns alert-box secondary');
+$attrs = array('id'=>'fvform', 'class'=>'columns alert-box secondary hidden');
 $hidden = array('fedid'=>'','provid'=>''.$providerid.'','fvid'=>'');
 echo '<div class="small-12 medium-10  columns">'.form_open(base_url().'federations/fvalidator/validate',$attrs,$hidden);
 echo '<div id="fvresult" style="display:none;" data-alert class="alert-box info"><div><b>'.lang('fvalidcodereceived').'</b>: <span id="fvreturncode"></span></div><div><p><b>'.lang('fvalidmsgsreceived').'</b>:</p><div id="fvmessages"></div></div></div>';
@@ -28,41 +29,19 @@ echo '<div class="buttons"><button type="submit" id="fvalidate" name="fvalidate"
 <?php
 if(!empty($showform))
 {
-    echo '<div class="small-12 columns">';
-
-
-    $buttons = '<div class="buttons small-9 columns text-right end"><button type="submit" name="modify" value="submit" class="savebutton saveicon">' . lang('rr_apply') . '</button></div>';
+    echo '<div class="small-12 column">';
+    $buttons = '<button type="submit" name="modify" value="submit" class="savebutton saveicon">' . lang('rr_apply') . '</button>';
     $form = form_open(null, array('id' => 'joinfed'));
-    $form .= form_fieldset(lang('joinfederation'));
     $form .= '<div class="small-12 columns"><div class="small-3 columns">';
     $form .= '<label for="fedid" class="right inline">' . lang('rr_selectfedtojoin') . '</label></div>';
-    $form .= '<div class="small-6 large-7 columns end">' . form_dropdown('fedid', $feds_dropdown, '0', 'id="fedid"') . '</div>';
+    $form .= '<div class="small-6 large-7 columns end">' . form_dropdown('fedid', $feds_dropdown, '0', 'id="fedid" class="select2"') . '</div>';
     $form .= '</div><div class="small-12 columns">';
     $form .= '<div class="small-3 columns"><label for="formmessage" class="inline right">' . lang('rr_message') . '</label></div>';
     $form .= '<div class="small-6 large-7 columns end">' . form_textarea('formmessage', set_value('formmessage')) . '</div>';
     $form .= '</div>';
-    $form .= form_fieldset_close();
-    $form .= $buttons;
+    $form .= '<div class="buttons small-12 medium-10 large-10 column end text-right  ">'.$buttons.'</div>';
     $form .= form_close();
-
     echo $form;
-
-
-
-
-
-
-
-
-
-
     echo '</div>';
-
-
-
-
-
-
 }
 echo '<div id="retrfvalidatorjson" style="display:none">'.base_url().'federations/fvalidator/detailjson</div>';
-?>
