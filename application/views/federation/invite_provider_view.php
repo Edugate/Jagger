6f97dec88a6024eba77db0d50a3f31b5ee8150fc
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2017-07-21 12:45:45 +0100

    fix form elements width

--- application/views/federation/invite_provider_view.php
+++ application/views/federation/invite_provider_view.php
@@ -10,26 +10,23 @@ if (!empty($providers)) {
         echo $errors_v;
         echo "</div>";
     }
-    echo form_fieldset(lang('rr_fedinvidpsp') . ': ' . $fedname);
-
     echo '<div class="small-12 columns">';
-    echo '<div class="small-3 columns">' . jform_label('' . lang('rrprovs') . '', 'provider') . '</div>';
-    echo '<div class="small-6 large-7 columns end">' . form_dropdown('provider', $providers, set_value('provider'),'class="select2"') . '</div>';
+    echo '<div class="small-12 medium-3 columns">' . jform_label('' . lang('rr_fedinvidpsp') . '', 'provider') . '</div>';
+    echo '<div class="small-12 medium-7 columns end">' . form_dropdown('provider', $providers, set_value('provider'),'class="select2"') . '</div>';
     echo "</div>";
     echo '<div class="small-12 columns">';
-    echo '<div class="small-3 columns">' . jform_label('' . lang('rr_message') . '', 'message') . '</div>';
-    echo '<div class="small-6 large-7 columns end">' . form_textarea('message') . '</div>';
+    echo '<div class="small-12 medium-3 columns">' . jform_label('' . lang('rr_message') . '', 'message') . '</div>';
+    echo '<div class="small-12 medium-7 columns end">' . form_textarea('message') . '</div>';
     echo "</div>";
-    echo form_fieldset_close();
     $buttons = array(
         '<a href="'.$fedurl.'" class="button alert">'.lang('rr_cancel').'</a>',
         '<button type="submit" name="submit" value="Invitation" class="button savebutton saveicon">'.lang('rr_submit').'</button>',
     );
-    echo '<div class="small-12 columns">';
+    echo '<div class="small-12 column">';
+    echo '<div class="small-12 medium-3 column"></div>';
+    echo '<div class="small-12 medium-10 column end">';
     echo revealBtnsRow($buttons);
     echo '</div>';
+    echo '</div>';
     echo form_close();
-
-
-
 }
