43e11d06776509951f6fb736976063b9529485c0
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2017-08-02 15:44:02 +0100

    statcollector workers added support for rabbitmq

--- application/libraries/Gearmanw.php
+++ application/libraries/Gearmanw.php
@@ -5,6 +5,7 @@ if (!defined('BASEPATH')) {
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
+
 /**
  * @package   Jagger
  * @author    Janusz Ulanowski <janusz.ulanowski@heanet.ie>
@@ -22,24 +23,48 @@ class Gearmanw
         $this->ci->load->helper('file');
     }
 
-    private static function fn_externalstatcollectionGearman(\GearmanJob $job) {
+    /**
+     * @param GearmanJob|\PhpAmqpLib\Message\AMQPMessage $job
+     * @return bool
+     */
+    private static function fn_externalstatcollection($job) {
+
 
         $ci = &get_instance();
-        log_message('info', 'GEARMAN ::' . __METHOD__ . ' received job');
+        log_message('info', __METHOD__ . ' received job');
         $em = $ci->doctrine->em;
-        $args = @unserialize($job->workload());
-        if (empty($args) || !is_array($args)) {
-            $args = json_decode($job->workload(), true);
+        $jobStatus = false;
+        if ($job instanceof GearmanJob) {
+            $args = @unserialize($job->workload());
             if (empty($args) || !is_array($args)) {
-                log_message('error', 'GEARMAN ::' . __METHOD__ . ' didnt received args from requester');
-                $em->clear();
+                $args = json_decode($job->workload(), true);
+                if (empty($args) || !is_array($args)) {
+                    log_message('error', 'GEARMAN ::' . __METHOD__ . ' didnt received args from requester');
+                    $em->clear();
 
-                return false;
+                    return false;
+                }
+                $jobStatus = true;
             }
+        } elseif ($job instanceof \PhpAmqpLib\Message\AMQPMessage) {
+            $args = @unserialize($job->body);
+            if (empty($args) || !is_array($args)) {
+                $args = json_decode($job->body, true);
+                if (empty($args) || !is_array($args)) {
+                    log_message('error', 'RABBIT ::' . __METHOD__ . ' didnt received args from requester');
+                    $em->clear();
+                    return false;
+                }
+            }
+        } else {
+            throw new Exception('incorrect instance');
         }
 
 
-        $job->sendStatus(1, 10);
+
+        if ($jobStatus) {
+            $job->sendStatus(1, 10);
+        }
         sleep(1);
         $storage = $ci->config->item('datastorage_path');
         $img_mimes = array(
@@ -51,14 +76,15 @@ class Gearmanw
         );
 
         if (empty($storage)) {
-            log_message('error', 'GEARMAN :: datastorage not found');
+            log_message('error', __METHOD__ . ' :: datastorage not found');
             $em->clear();
 
             return false;
         }
+
         $statstorage = $storage . 'stats/';
         if (!array_key_exists('defid', $args)) {
-            log_message('error', 'GEARMAN ::' . __METHOD__ . ' definition stat id not found in args');
+            log_message('error', __METHOD__ . ' ::' . __METHOD__ . ' definition stat id not found in args');
             $em->clear();
 
             return false;
@@ -67,15 +93,18 @@ class Gearmanw
         }
         $maxattempts = 2;
         $attempt = 0;
+        /**
+         * @var $def models\ProviderStatsDef
+         */
         while ($attempt < $maxattempts) {
             try {
                 $def = $em->getRepository("models\ProviderStatsDef")->findOneBy(array('id' => $args['defid'], 'type' => 'ext'));
                 $attempt = $maxattempts;
             } catch (Exception $e) {
                 if ($attempt < $maxattempts) {
-                    log_message('warning', 'GEARMAN ::' . __METHOD__ . ' lost connection to database trying to reconnect');
+                    log_message('warning', __METHOD__ . ' lost connection to database trying to reconnect');
                 } else {
-                    log_message('error', 'GEARMAN ::' . __METHOD__ . ' no connection to database');
+                    log_message('error', __METHOD__ . ' no connection to database');
                 }
                 $em->getConnection()->close();
                 $em->getConnection()->connect();
@@ -83,22 +112,27 @@ class Gearmanw
                 sleep(2);
             }
         }
-        if (empty($def)) {
+
+        if (null === $def) {
             log_message('error', 'GEARMAN ::' . __METHOD__ . ' defition stat not found with provider defid');
             $em->clear();
 
             return false;
         }
-        $job->sendStatus(1, 10);
+        if ($jobStatus) {
+            $job->sendStatus(1, 10);
+        }
         $provider = $def->getProvider();
 
-        if (empty($provider)) {
-            log_message('debug', 'GEARMAN ::' . __METHOD__ . ' statdefinition has no provider owner');
+        if ($provider === null) {
+            log_message('debug', __METHOD__ . ' statdefinition has no provider owner');
             $em->clear();
 
             return false;
         }
-        $job->sendStatus(2, 10);
+        if ($jobStatus) {
+            $job->sendStatus(2, 10);
+        }
         $expectedformat = $def->getFormatType();
         $overwrite = $def->getOverwrite();
         $s = null;
@@ -109,14 +143,13 @@ class Gearmanw
                 $filename = $s->getFilename();
             }
         }
-        $job->sendStatus(3, 10);
+        if ($jobStatus) {
+            $job->sendStatus(3, 10);
+        }
 
         $data = null;
         $method = $def->getHttpMethod();
-        $params = $def->getPostOptions();
-        if (empty($params) || !is_array($params)) {
-            $params = array();
-        }
+        $params = (array)$def->getPostOptions();
         $accesstype = $def->getAccessType();
 
         $ci->curl->create('' . $def->getSourceUrl() . '');
@@ -127,28 +160,31 @@ class Gearmanw
         if ($method === 'post') {
             $ci->curl->post($params);
         }
-        log_message('debug', 'GEARMAN ::' . __METHOD__ . ' executing curl');
+        log_message('debug', __METHOD__ . ' executing curl');
         $curltimeout = $ci->config->item('curltimeout');
+
         if (isset($curltimeout)) {
             $addoptions = array('TIMEOUT' => (int)$curltimeout);
-            log_message('debug', 'GEARMAN ::' . __METHOD__ . ' curl setting timeout: ' . (int)$curltimeout);
+            log_message('debug', __METHOD__ . ' curl setting timeout: ' . (int)$curltimeout);
             $ci->curl->options($addoptions);
         }
+
         $data = $ci->curl->execute();
+
         if (!empty($data)) {
-            log_message('debug', 'GEARMAN ::' . __METHOD__ . ' received data not empty');
+            log_message('debug', __METHOD__ . ' received data not empty');
             $job->sendStatus(5, 10);
             $finfo = new finfo(FILEINFO_MIME_TYPE);
             $mimeType = $finfo->buffer($data);
             if ($expectedformat === 'image') {
-                log_message('debug', 'GEARMAN ::' . __METHOD__ . ' mimetype of received data: ' . $mimeType . ' checking if allowed');
+                log_message('debug', __METHOD__ . ' mimetype of received data: ' . $mimeType . ' checking if allowed');
                 if (!array_key_exists($mimeType, $img_mimes)) {
-                    log_message('error', 'GEARMAN ::' . __METHOD__ . ' not allowed mimetype: ' . $mimeType);
+                    log_message('error', __METHOD__ . ' not allowed mimetype: ' . $mimeType);
                     $em->clear();
 
                     return false;
                 } else {
-                    log_message('debug', 'GEARMAN ::' . __METHOD__ . ' mimetype is allowed... processing');
+                    log_message('debug', __METHOD__ . ' mimetype is allowed... processing');
                     $extension = $img_mimes['' . $mimeType . ''];
                     $statformat = 'image';
                 }
@@ -156,22 +192,23 @@ class Gearmanw
                 $extension = 'svg';
                 $statformat = 'svg';
             } else {
-                log_message('error', 'GEARMAN ::' . __METHOD__ . ' not allowed mimetype: ' . $mimeType);
+                log_message('error', __METHOD__ . ' not allowed mimetype: ' . $mimeType);
             }
 
             sleep(1);
-            $job->sendStatus(7, 10);
+            if ($jobStatus) {
+                $job->sendStatus(7, 10);
+            }
 
             if (!empty($extension) && !empty($statformat)) {
                 if (empty($filename)) {
                     $filename = $provider->getId() . '_' . $def->getId() . '_' . mt_rand() . '.' . $extension;
                 }
                 if (!write_file($statstorage . $filename, $data)) {
-                    log_message('debug', 'GEARMAN ::' . __METHOD__ . ' coulnd write file ' . $statstorage . $filename . ' on disk');
+                    log_message('debug', __METHOD__ . ' coulnd write file ' . $statstorage . $filename . ' on disk');
 
                     return false;
                 } else {
-                    $job->sendStatus(8, 10);
                     $em->getConnection()->close();
                     $em->getConnection()->connect();
                     if (empty($s)) {
@@ -181,10 +218,11 @@ class Gearmanw
                         $st->setProvider($provider);
                         $st->setStatDefinition($def);
                         $em->persist($st);
-                        $job->sendStatus(9, 10);
                     } else {
                         $s->updateDate();
                         $em->persist($s);
+                    }
+                    if ($jobStatus) {
                         $job->sendStatus(9, 10);
                     }
                     try {
@@ -192,12 +230,13 @@ class Gearmanw
                     } catch (Exception $e) {
                         log_message('error', __METHOD__ . ' ' . $e);
                     }
-                    $job->sendStatus(10, 10);
+                    if($jobStatus) {
+                        $job->sendStatus(10, 10);
+                    }
                 }
             }
         } else {
-
-            log_message('debug', 'gworker no data stat retrieved');
+            log_message('error', __METHOD__.'::'.$ci->curl->error_string);
         }
 
         $em->clear();
@@ -206,30 +245,24 @@ class Gearmanw
         return true;
     }
 
-    private static function fn_externalstatcollectionRabbit($job) {
-    }
 
-    static public function fn_externalstatcollection($job) {
 
-        if ($job instanceof \GearmanJob) {
-            self::fn_externalstatcollectionGearman($job);
-        } else {
-            self::fn_externalstatcollectionRabbit($job);
-        }
-
-    }
 
     /**
      * @todo finish rabbit
      */
     private function registerRabbitCollectorWorkers() {
+        log_message('debug','Registering rabbit workers');
         $config = $this->ci->config->item('rabbitmq');
-        $connection = new AMQPStreamConnection($config['host'], $config['port'], $config['user'], $config['password']);
+        $vhost = '/';
+        if (isset($config['vhost'])) {
+            $vhost = $config['vhost'];
+        }
+        $connection = new AMQPStreamConnection($config['host'], $config['port'], $config['user'], $config['password'], $vhost);
         $channel = $connection->channel();
-        //$channel->queue_declare('hello', false, false, false, false);
 
         $callback = function ($msq) {
-            self::fn_externalstatcollectionRabbit($msq);
+            self::fn_externalstatcollection($msq);
         };
         $channel->basic_consume('externalstatcollection', '', false, true, false, false, $callback);
         while (count($channel->callbacks)) {
@@ -238,6 +271,9 @@ class Gearmanw
 
     }
 
+    /**
+     * @deprecated
+     */
     private function registerGearmanCollectorWorkers() {
         $gearmanConfig = $this->ci->config->item('gearmanconf');
         if (empty($gearmanConfig) || !isset($gearmanConfig['jobserver'])) {
@@ -267,7 +303,7 @@ class Gearmanw
                 }
             }
         }
-        while ($gm->work()) ;
+        while ($gm->work());
     }
 
     private function registerCollectorWorkers() {
