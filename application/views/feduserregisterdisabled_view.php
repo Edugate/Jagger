65ea81d3b93c91c7129389e9ebffaa8baaeae693
janusz <2247817+janul@users.noreply.github.com>
2022-05-04 22:23:43 +0100

    add option for customized notice

--- application/views/feduserregisterdisabled_view.php
+++ application/views/feduserregisterdisabled_view.php
@@ -1,9 +1,12 @@
-
+<?php
+$defaultMsg = "If you require an account to manage your entities please contact our support team.";
+$noticeMsg = $this->config->item('req_fedaccount_notice') ?: $defaultMsg;
+?>
 <h3><?php echo lang('rr_feduser_register_title');?></h3>
 <div class="alert-box alert"><?php echo lang('error_usernotexist');?></div>
 <br />
 <br />
-<div class="result notice">If you require an account to manage your entities please contact our support team.</div>
+<div class="result notice"><?php echo html_escape($noticeMsg);  ?></div>
 <br />
 
 
